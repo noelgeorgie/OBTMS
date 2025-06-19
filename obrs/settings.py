9dc436b08c104f1cc8664cf6ae0ee366f78711c0
Noel Abraham <noelegeorgie@gmail.com>
2025-06-19 18:15:49 +0530

    Update settings.py

--- obrs/settings.py
+++ obrs/settings.py
@@ -19,7 +19,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'agyzay4@gmail.com'
+EMAIL_HOST_USER = 'agyza@gmail.com'
 EMAIL_HOST_PASSWORD = "sfzh wotd bvtf exli"
 
 
